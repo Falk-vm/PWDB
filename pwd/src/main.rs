3aed04c839cdb48eb9c9d1131a8465b871dccc13
Falkvm <f.f.vonmaltzahn@gmail.com>
2022-11-04 12:30:12 +0100

    lol

--- pwd/src/main.rs
+++ pwd/src/main.rs
@@ -1,35 +1,46 @@
 use rand::Rng;
-use std::fs::OpenOptions;
-use std::io::Write;
-use fltk::{app, button::Button, frame::Frame, prelude::*, window::Window};
+//use std::fs::OpenOptions;
+//use std::io::Write;
+use fltk::{app,input, button::Button, frame::Frame, prelude::*, window::Window};
 
 fn main() {
     let app = app::App::default();
     let mut wind = Window::new(100, 100, 400, 300, "Password Generator");
     let mut frame = Frame::new(0, 0, 400, 200, "");
-    let mut but = Button::new(160, 210, 80, 40, "Click me!");
+    let mut but = Button::new(160, 210, 80, 40, "Generate");
+    let mut inp = input::Input::default().with_size(200, 40).with_label("Length").center_of_parent();
     wind.end();
     wind.show();
-    but.set_callback(move |_| frame.set_label("Hello World!")); // the closure capture is mutable borrow to our button
+    but.set_callback(move |_| {
+        let passw = gen(inp.value());
+        frame.set_label(&passw);
+    }); // the closure capture is mutable borrow to our button
     app.run().unwrap();
 }
 
-fn gen() -> String {
+fn gen(password_len: String) -> String {
+    let my_int: i32 = password_len.parse().unwrap();
     const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
                             abcdefghijklmnopqrstuvwxyz\
                             0123456789)(*&^%$#@!~";
-    const PASSWORD_LEN: usize = 8;
     let mut rng = rand::thread_rng();
-    let mut pwd_vec = Vec::new();
+    let mut pwd_str = String::new();
     for _i in 0..1 {
-        let password: String = (0..PASSWORD_LEN)
+        let password: String = (0..my_int)
             .map(|_| {
                 let idx = rng.gen_range(0..CHARSET.len());
                 CHARSET[idx] as char
             })
             .collect();
-        pwd_vec.push(password)
-    }/*
+        pwd_str.push_str(&password);
+    }
+    return pwd_str;
+}
+
+
+
+/* unused/obsulete code
+fn write_to_file() {
     let mut file = OpenOptions::new()
         .write(true)
         .append(true)
@@ -37,6 +48,5 @@ fn gen() -> String {
         .unwrap();
         
     file.write_all(pwd_vec.join("\n").as_bytes()).unwrap();
+    }
     */
-    return pwd_vec
-}
---
+++ pwd/target/debug/deps/pwd-99bdafd1d5190b78.d
@@ -0,0 +1,5 @@
+C:\Users\Falk\Documents\PWDB\pwd\target\debug\deps\pwd-99bdafd1d5190b78.exe: src\main.rs
+
+C:\Users\Falk\Documents\PWDB\pwd\target\debug\deps\pwd-99bdafd1d5190b78.d: src\main.rs
+
+src\main.rs:
